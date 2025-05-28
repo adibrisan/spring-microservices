b4bcd1bffbc34818bcfa29e72efb199909989ebb
adibrisan <adi.brisan@yahoo.com>
2025-05-28 21:01:31 +0300

    Send a simple message from notification service when the app starts

--- notification/src/main/java/com/adimicroservices/notification/NotificationApplication.java
+++ notification/src/main/java/com/adimicroservices/notification/NotificationApplication.java
@@ -3,9 +3,23 @@ package com.adimicroservices.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.adimicroservices.notification",
+                "com.adimicroservices.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+//        return args -> {
+//            producer.publish(new Person("adrian",26),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//    record Person(String name, int age){}
 }
